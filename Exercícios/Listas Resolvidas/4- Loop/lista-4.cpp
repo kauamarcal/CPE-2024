775bd1d41cea75247ea55d8e1d1758bd5a21d3dd
kauamarcal <kaua.marcal.araujo@gmail.com>
2024-04-17 14:30:59 -0300

    changes

--- Exercícios/Listas Resolvidas/4- Loop/lista-4.cpp
+++ Exercícios/Listas Resolvidas/4- Loop/lista-4.cpp
@@ -1,9 +1,80 @@
-#include <iostream>
-using namespace std;
+/*1 - O fatorial de um número inteiro não negativo n é escrito como n! (pronunciado “n fatorial”) e é
+definido do seguinte modo: para valores , para
+. Por exemplo, , que é 120. Os fatoriais aumentam de tamanho
+muito rapidamente. Qual é o maior fatorial que seu programa pode calcular antes de gerar um
+estouro de memória?
+A) Escreva um programa que leia um número inteiro não negativo e calcule e imprima seu
+fatorial.b
+B) Escreva um programa que calcule o valor da constante matemática usando a seguinte
+fórmula (Nota: Seu código pode parar após somar 10 termos.)
+C) Escreva um programa que calcule o valor da constante matemática usando a seguinte fórmula
+(Nota: Seu código pode parar após somar 10 termos.)*/
 
-//Programa que calcula uma divisão Inteira
-int main(){
-	cout << "dividendo:"; // imprime o texto na tela
-	
-	return 0;
-} 
+
+/*2 - Escreva um programa que imprima os seguintes padrões separadamente, um abaixo do outro,
+cada padrão separado do próximo por uma linha em branco. Use laços for para gerar os padrões.
+Todos os asteriscos (*) devem ser impressos por uma única declaração na forma: cout << ‘*’ ;
+o que faz com que os asteriscos imprimam lado a lado, uma declaração no formato cout << '\n';
+pode ser usado para passar para a próxima linha. Uma instrução no formato cout << ' '; pode ser
+usado para exibir um espaço para os dois últimos padrões. (Dica: os dois últimos padrões exigem
+que cada linha comece com um número apropriado de espaços em branco.) Crédito extra: combine
+seu código dos quatro problemas separados em um único programa que imprime todos os quatro
+padrões lado a lado, fazendo um uso inteligente dos laços aninhados for. Para todas as partes deste
+exercício - minimize o número de asteriscos e espaços e o número de instruções que imprimem
+esses caracteres.*/
+
+
+/*3 - (Triplos de pitágoras) Um triângulo retângulo pode ter lados que são todos inteiros. O conjunto
+de três valores inteiros para os lados de um triângulo retângulo é chamado de triplo de pitágoras.
+Esses três lados devem satisfazer a relação de que a soma dos quadrados de dois dos lados é igual
+ao quadrado da hipotenusa. Encontre todos os triplos pitagóricos para lado1, lado2 e hipotenusa,
+todos com tamanho não superior a 20. Use um laço for triplamente aninhado que tente todas as
+possibilidades. Este é um exemplo de computação de "força bruta". Você aprenderá em cursos de
+ciência da computação mais avançados que existem muitos problemas interessantes para os quais
+não existe uma abordagem algorítmica conhecida além da pura força bruta. */
+
+
+/*4 - (Cálculo de vendas) Um varejista on-line vende cinco produtos cujos preços de varejo são os
+seguintes: Produto 1, R$ 2,98; produto 2, R$ 4,50; produto 3, R$ 9,98; produto 4, R$ 4,49 e produto
+5, R$ 6,87. Escreva um aplicativo que leia uma série de pares de números da seguinte forma:
+A. número do produto
+B. quantidade vendida
+Seu programa deve usar uma instrução switch para determinar o preço de varejo para cada produto.
+Deve calcular e exibir o valor total de varejo de todos os produtos vendidos. Use um laço 'while'
+para determinar quando o programa deve parar e exibir os resultados finais.
+n! = n × (n − 1) × (n − 2) × . . .1 n ≥ 1 n! = 1
+n = 0 5! = 5 × 4 × 3 × 2 × 1
+e
+e = 1 + 1
+1!
++ 1
+2!
++ 1
+3!
++ . . .
+ex
+ex = 1 +
+x
+1!
++
+x2
+2!
++
+x3
+3!
++ . . .
+*/
+
+
+/*5 - (Programa de impressão de gráficos de barras) Uma aplicação interessante dos computadores é
+exibir gráficos e tabelas de barras. Escreva um aplicativo que leia cinco números entre 1 e 30. Para
+cada número lido, seu programa deverá exibir o mesmo número de asteriscos adjacentes. Por
+exemplo, se o seu programa lê o número 7, ele deverá exibir ******* . Mostrar as barras de
+asteriscos depois de ler todos os cinco números.*/
+
+
+/*6 - Escreva um programa para jogar um jogo de adivinhação de números. O usuário pensa em um
+número entre 1 e 100 e seu programa faz perguntas para descobrir qual é o número (por exemplo,
+“O número em que você está pensando é menor que 50?”). Seu programa deve ser capaz de
+identificar o número depois de fazer no máximo sete perguntas. Dica: Use os operadores <, <=, a
+construção if-else e a função rand caso tenha dúvida. */
